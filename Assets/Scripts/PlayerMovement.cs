48da84074088a072442fb775d78f6fdd312a0063
Mike Harris <jammin804@gmail.com>
2025-08-25 20:30:10 -0400

    Complete Player Movement and Jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+   [SerializeField] private Rigidbody2D _rb;
+   [SerializeField] private float _moveSpeed = 5f;
+   [SerializeField] private float _jumpForce = 10f;
+   [SerializeField] private Transform _groundCheck;
+   [SerializeField] private Vector2 _groundCheckSize= new Vector2(.5f, .05f);
+   [SerializeField] private LayerMask _groundMask;
+   
+   private float _horizontalMovement = 0f;
+   
+
+   private void Update()
+   {
+      _rb.linearVelocity = new Vector2(_horizontalMovement * _moveSpeed, _rb.linearVelocity.y);
+      // _rb.AddForce(Vector2.up * _moveSpeed, ForceMode2D.Impulse);
+   }
+
+   public void Move(InputAction.CallbackContext context)
+   {
+      _horizontalMovement = context.ReadValue<Vector2>().x;
+      
+   }
+
+   public void Jump(InputAction.CallbackContext context)
+   {
+      if (IsGrounded())
+      {
+         if (context.performed)
+         {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _jumpForce);
+         }
+      }
+
+
+   }
+
+   public bool IsGrounded()
+   {
+      if (Physics2D.OverlapBox(_groundCheck.position, _groundCheckSize, 0, _groundMask))
+      {
+         return true;
+      }
+      return false;
+   }
+
+   public void OnDrawGizmosSelected()
+   {
+      Gizmos.color = Color.white;
+      Gizmos.DrawWireCube(_groundCheck.position, _groundCheckSize);
+   }
+}
